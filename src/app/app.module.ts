c64acb677c4166487d4108b4fc1c0db1f30d7319
rgonzalez <rgonzalez@mkdautomotive.com>
2022-07-07 23:34:34 +0200

    Criterio 2 - Creación del componente blog y cargarlo dentro de la aplicación principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { FormComponent } from './components/form/form.component';
+import { ListComponent } from './components/list/list.component';
+import { ListItemComponent } from './components/list-item/list-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogComponent,
+    FormComponent,
+    ListComponent,
+    ListItemComponent
   ],
   imports: [
     BrowserModule
